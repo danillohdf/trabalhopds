78f780ffae29f94f6030d67d8c4d77dac0fd0bd6
João Marcos Santos Pereira <impgsolari@gmail.com>
2024-08-11 23:54:02 -0300

    Criar estrutura padrão do projeto

---
+++ include/errors.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef ERRORS_h
+#define ERRORS_h
+
+/* Constantes */
+#define SUCESSO     (0)
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "include/errors.h"
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    return SUCESSO;
+}
